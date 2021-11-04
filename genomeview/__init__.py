2a6ffd487033a096775d1752950745509f812900
danledinh <danledinh@users.noreply.github.com>
2021-11-03 20:48:52 -0700

    Update __init__.py

--- genomeview/__init__.py
+++ genomeview/__init__.py
@@ -7,17 +7,17 @@ try:
     from genomeview._quickconsensus import *
 except ImportError:
     import logging
-    logging.warn("Unable to load cythonized quickconsensus module; "
-                 "drawing reads using quick consensus mode will be "
-                 "substantially slower")
+#    logging.warn("Unable to load cythonized quickconsensus module; "
+#                 "drawing reads using quick consensus mode will be "
+#                 "substantially slower")
     from genomeview.quickconsensus import *
 except:
     import logging
-    logging.error("Unable to load cythonized quickconsensus module; "
-                  "this is likely because pysam has been updated "
-                  "since genomeview was originally install. To "
-                  "fix this, force reinstall genomeview: \n"
-                  "  pip install --upgrade --force-reinstall genomeview")
+#    logging.error("Unable to load cythonized quickconsensus module; "
+#                  "this is likely because pysam has been updated "
+#                  "since genomeview was originally install. To "
+#                  "fix this, force reinstall genomeview: \n"
+#                  "  pip install --upgrade --force-reinstall genomeview")
 
 from genomeview.axis import *
 from genomeview.track import *
